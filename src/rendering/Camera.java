f68171d5a90408af2fcaaa14551df17bf83700c1
Paul Huber <34043602+Paul-Huber@users.noreply.github.com>
2022-09-12 18:35:01 -0700

    Create Camera.java

---
+++ src/rendering/Camera.java
@@ -0,0 +1,49 @@
+package rendering;
+
+import org.joml.Vector3f;
+
+public class Camera {
+	private Vector3f position;
+	private float rotX;
+	private float rotY;
+	private float rotZ;
+	
+	public Camera(Vector3f position, float rotX, float rotY, float rotZ) {
+		this.position = position;
+		this.rotX = rotX;
+		this.rotY = rotY;
+		this.rotZ = rotZ;
+	}
+
+	public Vector3f getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector3f position) {
+		this.position = position;
+	}
+
+	public float getRotX() {
+		return rotX;
+	}
+
+	public void setRotX(float rotX) {
+		this.rotX = rotX;
+	}
+
+	public float getRotY() {
+		return rotY;
+	}
+
+	public void setRotY(float rotY) {
+		this.rotY = rotY;
+	}
+
+	public float getRotZ() {
+		return rotZ;
+	}
+
+	public void setRotZ(float rotZ) {
+		this.rotZ = rotZ;
+	}
+}
